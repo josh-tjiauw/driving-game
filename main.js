1aa88adb10e05bfc8b27fe4ac5c37cad6138a528
Josh Tjiauw <freezingmist2@gmail.com>
2020-12-11 12:54:50 -0800

    Added event listeners for the arrow keys

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
 var $car = document.getElementById('car');
 console.log($car);
+document.addEventListener('keydown', function(event){
+  if(event.code === 'ArrowUp'){
+    console.log('Hi');
+  }
+  else if(event.code === 'ArrowDown'){
+    console.log('Hello');
+  }
+  else if (event.code === 'ArrowLeft') {
+    console.log('Hey');
+  }
+  else if (event.code === 'ArrowRight') {
+    console.log('Her');
+  }
+  else {
+    return;
+  }
+});
