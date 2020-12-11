344a4af9f85d84f4084cfec3a90304add84f55b4
Josh Tjiauw <freezingmist2@gmail.com>
2020-12-11 13:20:31 -0800

    Added data model for location with x&y coordinates.

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 var $car = document.getElementById('car');
 console.log($car);
-document.addEventListener('keydown', function(event){
-  if(event.code === 'ArrowUp'){
+document.addEventListener('keydown', function (event) {
+  if (event.code === 'ArrowUp') {
     $car.style.transform = "rotate(270deg)"
   }
-  else if(event.code === 'ArrowDown'){
+  else if (event.code === 'ArrowDown') {
     $car.style.transform = "rotate(90deg)"
   }
   else if (event.code === 'ArrowLeft') {
@@ -13,7 +13,16 @@ document.addEventListener('keydown', function(event){
   else if (event.code === 'ArrowRight') {
     $car.style.transform = "rotate(0deg)"
   }
+  else if (event.code === 'Space') {
+  }
   else {
     return;
   }
 });
+
+var data = {
+  location: {
+    x: 0,
+    y: 0
+  }
+};
