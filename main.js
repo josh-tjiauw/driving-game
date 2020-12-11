230bcc72a6c1ae2660af988c693e5fe36f90b764
Josh Tjiauw <freezingmist2@gmail.com>
2020-12-11 15:54:53 -0800

    Added conditionals within the arrow key event listeners checking if the car is started

--- main.js
+++ main.js
@@ -3,15 +3,27 @@ console.log($car);
 document.addEventListener('keydown', function (event) {
   if (event.code === 'ArrowUp') {
     $car.style.transform = "rotate(270deg)"
+    if(data.started === true){
+      move('up');
+    }
   }
   else if (event.code === 'ArrowDown') {
     $car.style.transform = "rotate(90deg)"
+    if (data.started === true) {
+      move('down');
+    }
   }
   else if (event.code === 'ArrowLeft') {
     $car.style.transform = "rotate(180deg)"
+    if (data.started === true) {
+      move('left');
+    }
   }
   else if (event.code === 'ArrowRight') {
     $car.style.transform = "rotate(0deg)"
+    if (data.started === true) {
+      move('right');
+    }
   }
   else if (event.code === 'Space' && data.started === false) {
     move();
@@ -29,18 +41,51 @@ var data = {
   started: false
 };
 
-var moveCar;
+var moveCarRight;
+var moveCarLeft;
+var moveCarUp;
+var moveCarDown;
 
-function move(){
-  $car.style.top = data.location.y;
+function move(direction){
   data.started = true;
-  moveCar = setInterval(function(){
-    data.location.x++;
-    $car.style.left = data.location.x.toString().concat('px');
-  }, 16)
+  if(direction === 'right'){
+    clearInterval(moveCarLeft, moveCarDown, moveCarUp)
+    moveCarRight = setInterval(function () {
+      data.location.x++;
+      $car.style.top = data.location.y.toString().concat('px');
+      $car.style.left = data.location.x.toString().concat('px');
+    }, 16)
+  }
+
+  if(direction === 'left'){
+    clearInterval(moveCarDown, moveCarRight, moveCarUp)
+    moveCarLeft = setInterval(function () {
+      data.location.x--;
+      $car.style.top = data.location.y.toString().concat('px');
+      $car.style.left = data.location.x.toString().concat('px');
+    }, 16)
+  }
+
+  if(direction === 'up'){
+    clearInterval(moveCarLeft, moveCarRight, moveCarDown)
+    moveCarUp = setInterval(function () {
+      data.location.y--;
+      $car.style.top = data.location.y.toString().concat('px');
+      $car.style.left = data.location.x.toString().concat('px');
+    }, 16)
+  }
+
+  if(direction === 'down'){
+    clearInterval(moveCarLeft, moveCarRight, moveCarUp)
+    moveCarDown = setInterval(function () {
+      data.location.y++;
+      $car.style.top = data.location.y.toString().concat('px');
+      $car.style.left = data.location.x.toString().concat('px');
+    }, 16)
+  }
 }
 
 function stop(){
-  clearInterval(moveCar);
+  clearInterval(moveCarRight, moveCarLeft, moveCarUp, moveCarDown);
   data.started = false;
 }
