bb92f07b7de31640d4459591676ab40298779c1d
Josh Tjiauw <freezingmist2@gmail.com>
2020-12-11 17:53:15 -0800

    Trimmed the code a bit and transferred functionality from the event listener to the function move instead

--- main.js
+++ main.js
@@ -3,31 +3,15 @@ var direction;
 document.addEventListener('keydown', function (event) {
   if (event.code === 'ArrowUp' && direction !== 'up') {
     direction = 'up';
-    $car.style.transform = "rotate(270deg)"
-    if(data.started === true){
-      move('up');
-    }
   }
   else if (event.code === 'ArrowDown' && direction !== 'down') {
     direction = 'down';
-    $car.style.transform = "rotate(90deg)"
-    if (data.started === true) {
-      move('down');
-    }
   }
   else if (event.code === 'ArrowLeft' && direction !== 'left') {
     direction = 'left';
-    $car.style.transform = "rotate(180deg)"
-    if (data.started === true) {
-      move('left');
-    }
   }
   else if (event.code === 'ArrowRight' && direction !== 'right') {
     direction = 'right';
-    $car.style.transform = "rotate(0deg)"
-    if (data.started === true) {
-      move('right');
-    }
   }
   else if (event.code === 'Space' && data.started === false) {
     move();
@@ -45,62 +29,34 @@ var data = {
   started: false
 };
 
-var moveCarRight;
-var moveCarLeft;
-var moveCarUp;
-var moveCarDown;
+var moveCar;
 
-function move(direction){
+function move(event){
   data.started = true;
-  if(direction === 'right'){
-    clearInterval(moveCarLeft);
-    clearInterval(moveCarDown);
-    clearInterval(moveCarUp);
-    moveCarRight = setInterval(function () {
-      data.location.x+=10;
-      $car.style.top = data.location.y.toString().concat('px');
-      $car.style.left = data.location.x.toString().concat('px');
-    }, 16)
-  }
-
-  if(direction === 'left'){
-    clearInterval(moveCarRight);
-    clearInterval(moveCarDown);
-    clearInterval(moveCarUp);
-    moveCarLeft = setInterval(function () {
-      data.location.x-=10;
-      $car.style.top = data.location.y.toString().concat('px');
-      $car.style.left = data.location.x.toString().concat('px');
-    }, 16)
-  }
-
-  if(direction === 'up'){
-    clearInterval(moveCarLeft);
-    clearInterval(moveCarRight);
-    clearInterval(moveCarDown);
-    moveCarUp = setInterval(function () {
-      data.location.y-=10;
-      $car.style.top = data.location.y.toString().concat('px');
-      $car.style.left = data.location.x.toString().concat('px');
-    }, 16)
-  }
-
-  if(direction === 'down'){
-    clearInterval(moveCarLeft);
-    clearInterval(moveCarRight);
-    clearInterval(moveCarUp);
-    moveCarDown = setInterval(function () {
-      data.location.y+=10;
-      $car.style.top = data.location.y.toString().concat('px');
-      $car.style.left = data.location.x.toString().concat('px');
-    }, 16)
-  }
+  clearInterval(moveCar);
+  moveCar = setInterval(function () {
+    if(direction === 'right'){
+      data.location.x += 10;
+      $car.style.transform = "rotate(0deg)"
+    }
+    else if(direction === 'left'){
+      data.location.x -= 10;
+      $car.style.transform = "rotate(180deg)"
+    }
+    else if(direction === 'up'){
+      data.location.y -= 10;
+      $car.style.transform = "rotate(270deg)"
+    }
+    else if(direction === 'down'){
+      data.location.y += 10;
+      $car.style.transform = "rotate(90deg)"
+    }
+    $car.style.top = data.location.y.toString().concat('px');
+    $car.style.left = data.location.x.toString().concat('px');
+  }, 16)
 }
 
 function stop(){
-  clearInterval(moveCarLeft);
-  clearInterval(moveCarRight);
-  clearInterval(moveCarUp);
-  clearInterval(moveCarDown);
+  clearInterval(moveCar);
   data.started = false;
 }
